5a4d8351d3dab10113af8b6e621bc6f69054bfd1
danugarri <danugarri@hotmail.com>
2023-07-22 20:19:15 +0200

    Split code into a utils module and generate each function

--- python/index_3.py
+++ python/index_3.py
@@ -1,7 +1,5 @@
-import random  # Module to generate random numbers
-
 # Importing functions from utils
-from utils import print_matrix
+from utils import populate_matrix, print_matrix, rows_sumation
 
 
 def generate_matrix():
@@ -15,32 +13,12 @@ def generate_matrix():
     if typed_input <= 0:
         print("The typed input must be bigger than 0.")
         return
-
-    # Initialize matrix
-    matrix = []
-
-    # Matrix population
-    for row in range(typed_input):
-        row_array = []
-        # Loop for column entries
-        for column in range(typed_input):
-            # Adding random numbers between 0 an 9 inculded
-            row_array.append(random.randint(0, 9))
-        matrix.append(row_array)
-
+    # Initialise and populate Matrix
+    matrix = populate_matrix(typed_input)
     # For printing the matrix
     print_matrix(typed_input, matrix)
     # Rows sumation
-    print("\nRows sumation")
-    rows_array_sumation = []
-    # Adding row_index
-    for row_index, row in enumerate(matrix):
-        # Each row sumation
-        row_sum = sum(row)
-        # Appending each sumation to the Rows array sumation
-        rows_array_sumation.append(row_sum)
-        print("Row", row_index, ":", row, "-> Sum:", row_sum)
-    print("\nRows Array sumation:", rows_array_sumation)
+    rows_sumation(matrix)
 
 
 generate_matrix()
--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,37 @@
+import random  # Module to generate random numbers
+
+
+def populate_matrix(typed_input):
+    # Initialize matrix
+    matrix = []
+
+    # Matrix population
+    for row in range(typed_input):
+        row_array = []
+        # Loop for column entries
+        for column in range(typed_input):
+            # Adding random numbers between 0 an 9 inculded
+            row_array.append(random.randint(0, 9))
+        matrix.append(row_array)
+    return matrix
+
+
 def print_matrix(typed_input, matrix):
     for row in range(typed_input):
         print("|\t", end="")
         for column in range(typed_input):
             print(matrix[row][column], end="\t")
         print("|")
+
+
+def rows_sumation(matrix):
+    print("\nRows sumation")
+    rows_array_sumation = []
+    # Adding row_index
+    for row_index, row in enumerate(matrix):
+        # Each row sumation
+        row_sum = sum(row)
+        # Appending each sumation to the Rows array sumation
+        rows_array_sumation.append(row_sum)
+        print("Row", row_index, ":", row, "-> Sum:", row_sum)
+    print("\nRows Array sumation:", rows_array_sumation)
