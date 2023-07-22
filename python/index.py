c2d6cb2b8b94a4ba85b4496e71d2d63ea70dbb93
danugarri <danugarri@hotmail.com>
2023-07-22 18:28:05 +0200

    Added exception handling

--- python/index.py
+++ python/index.py
@@ -1,22 +1,32 @@
 def generate_matrix():
-    typed_input= int(input("Enter the number of row and columns:"))
+    try:
+        # Exception controller
+        typed_input = int(input("Enter the number of rows and columns:"))
+    except ValueError:
+        print("Invalid input type. Please enter a positive integer not a letter.")
+        return
+
+    if typed_input <= 0:
+        print("The typed input must be bigger than 0.")
+        return
 
     # Initialize matrix
     matrix = []
-  
+
     # Matrix population
     for row in range(typed_input):
-        row_array=[]
+        row_array = []
         # A for loop for column entries
         for column in range(typed_input):
             row_array.append(0)
-            matrix.append(row_array)
+        matrix.append(row_array)
 
     # For printing the matrix
     for row in range(typed_input):
-        print("|\t" , end="")
+        print("|\t", end="")
         for column in range(typed_input):
-            print(matrix[row][column], end='\t' )
+            print(matrix[row][column], end="\t")
         print("|")
+
 
 generate_matrix()
