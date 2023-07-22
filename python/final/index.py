63ccd709b0a2eb032f4a9167749180ed0c38a87e
danugarri <danugarri@hotmail.com>
2023-07-22 20:33:33 +0200

    Created the final folder

---
+++ python/final/index.py
@@ -0,0 +1,18 @@
+# Importing functions from utils
+from utils import exception_handler, populate_matrix, print_matrix, rows_sumation
+
+
+def generate_matrix():
+    typed_input = exception_handler()
+    if typed_input is None:
+        print("Invalid input or value not provided. Exiting....")
+        return
+    # Initialise and populate Matrix
+    matrix = populate_matrix(typed_input)
+    # For printing the matrix
+    print_matrix(typed_input, matrix)
+    # Rows sumation
+    rows_sumation(matrix)
+
+
+generate_matrix()
