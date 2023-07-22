e261030781ea9001d1e2cd6a3612f6fce1a322a4
danugarri <danugarri@hotmail.com>
2023-07-22 17:33:30 +0200

    Added python first approach

---
+++ python/index.py
@@ -0,0 +1,21 @@
+def generate_matrix():
+    typed_input= int(input("Enter the number of row and columns:"))
+
+    # Initialize matrix
+    matrix = []
+  
+    # Matrix population
+    for row in range(typed_input):
+        row_array=[]
+        # A for loop for column entries
+        for column in range(typed_input):
+            row_array.append(0)
+            matrix.append(row_array)
+
+    # For printing the matrix
+    for row in range(typed_input):
+        for column in range(typed_input):
+            print(matrix[row][column], end=" ")
+        print()
+
+generate_matrix()
