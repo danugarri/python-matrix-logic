68a567c7262cecda105eadbb64c397dcd60ca391
danugarri <danugarri@hotmail.com>
2023-07-24 01:26:59 +0200

    Added another test case for populate_atrix function

---
+++ python/final/test_utils_exception_handler.py
@@ -0,0 +1,27 @@
+import unittest
+from utils import exception_handler
+from unittest.mock import patch
+
+# Constant definition
+BUILTIN_INPUT = "builtins.input"
+
+
+class TestExceptionHandler(unittest.TestCase):
+    def test_negative_inputs(self):
+        print("\nTesting negative inputs for matrix:")
+        # Test with a negative input
+        with patch(BUILTIN_INPUT, return_value="-3"):
+            result = exception_handler()
+        self.assertIsNone(result)
+
+        print("\nTesting 0 input value for matrix")
+        # Test with a 0 input value
+        with patch(BUILTIN_INPUT, return_value="0"):
+            result = exception_handler()
+        self.assertIsNone(result)
+
+        print("\nTesting letters inputs for matrix")
+        # Test with a negative input
+        with patch(BUILTIN_INPUT, return_value="f"):
+            result = exception_handler()
+        self.assertIsNone(result)
