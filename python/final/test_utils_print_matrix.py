71a87bc0e6f61f7f6a5632a004a8d37f4540fa3e
danugarri <danugarri@hotmail.com>
2023-07-24 01:53:10 +0200

    Added another test case for print_matrix

---
+++ python/final/test_utils_print_matrix.py
@@ -0,0 +1,36 @@
+import unittest
+import io
+import sys
+from utils import print_matrix
+from test_utils_polulate_matrix import TYPED_INPUT
+
+# Constant definition
+BUILTIN_INPUT = "builtins.input"
+
+
+class TestExceptionHandler(unittest.TestCase):
+    # This function is a built in one
+    def setUp(self):
+        print("\nTesting printed matrix:")
+        # Setting up a mocked matrix of 4*4
+        self.typed_input = TYPED_INPUT
+        self.matrix = [[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 9], [0, 1, 2, 3]]
+
+    def capture_print_output(self):
+        # Captures the output of the print statements and returns them as a string
+        output_buffer = io.StringIO()
+        sys.stdout = output_buffer
+        return output_buffer
+
+    def test_print_matrix(self):
+        # Test the function with the provided matrix
+        expected_output = "\n\n|\t1\t2\t3\t4\t|\n|\t4\t5\t6\t7\t|\n|\t7\t8\t9\t9\t|\n|\t0\t1\t2\t3\t|\n"
+
+        output_buffer = self.capture_print_output()  # Call to our defined method
+        print_matrix(self.typed_input, self.matrix)
+        sys.stdout = sys.__stdout__  # Restore the default stdout
+
+        self.assertEqual(output_buffer.getvalue(), expected_output)
+        print(
+            "stdout output: ", output_buffer.getvalue(), "\nEquals\n:", expected_output
+        )
